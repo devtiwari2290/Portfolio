588b361559f3da40fd1df04e0ec5559067ec2cca
Dev Tiwari <devtiwari2290@gmail.com>
2024-09-13 13:46:38 +0530

    new commit

--- src/components/MiniHome.jsx
+++ src/components/MiniHome.jsx
@@ -2,85 +2,102 @@ import React, { useEffect, useRef, useState } from "react";
 import { useGSAP } from "@gsap/react";
 import { gsap } from "gsap";
 import { motion } from "framer-motion";
-import "./App.css"
-import Pic from "../assets/Profile3.webp"
-
+import "./App.css";
+import Pic from "../assets/Profile3.webp";
 
 function Home() {
-
-  if(typeof window !== "undefined"){
+  if (typeof window !== "undefined") {
     window.scrollTo(0, 0);
 
-    useGSAP(()=>{
-      gsap.to(".marque",{
-        scrollTrigger:{
-          trigger: '.marque',
+    useGSAP(() => {
+      gsap.to(".marque", {
+        scrollTrigger: {
+          trigger: ".marque",
           start: "top 50%",
           end: "bottom 50%",
           scrub: 1,
-        }
-      })
+        },
+      });
 
-      gsap.to(".marque",{
+      gsap.to(".marque", {
         duration: 5,
-        transform : "translateX(-100%)",
+        transform: "translateX(-100%)",
         repeat: -1,
-        ease:"none"
-      })
+        ease: "none",
+      });
 
-        gsap.to(".marque img",{
-          rotate: 180
-        })
-    }) 
+      gsap.to(".marque img", {
+        rotate: 180,
+      });
+    });
+  } else {
+    useGSAP(() => {
+      gsap.to(".marque", {
+        duration: 5,
+        transform: "translateX(0%)",
+        repeat: -1,
+        ease: "none",
+      });
+
+      gsap.to(".marque img", {
+        rotate: 0,
+      });
+    });
   }
-
-  else{
-    useGSAP(()=>{
-      gsap.to(".marque",{
-         duration: 5,
-         transform : "translateX(0%)",
-         repeat: -1,
-         ease:"none"
-        });
-
-        gsap.to(".marque img",{
-          rotate: 0
-        })
-      
-    })
-
-   
-  }
-
-
 
   return (
     <div className="home w-full max-h-screen bg-[#111111] pt-[100px] relative z-[-10] lg:pt-[150px]">
-
       <div className="max-w-screen h-[300px]  px-8  object-cover lg:px-0 lg:w-[800px] lg:h-[400px] rounded-lg object-cover   lg:object-contain  mx-auto ">
-        <img  className="max-w-full h-full mx-auto object-cover  lg:object-contain " src={Pic} alt="" />
-        </div>
+        <img
+          className="max-w-full h-full mx-auto object-cover  lg:object-contain "
+          src={Pic}
+          alt=""
+        />
+      </div>
       <div className="move bg-[#D8FF08] flex shrink py-2 overflow-hidden mt-10  lg:mt-10">
         <div className="marque flex shrink-0 justify-center items-center gap-10  py-2 px-5 lg:py-5 px-10 ">
-          <h1 className=" text-lg lg:text-5xl text-center font-semibold" style={{ fontFamily: "Saira Condensed, sans-serif" }}>WELCOME TO MY PORTFOLIO</h1>
-          <img className="h-[20px] lg:h-[50px]" src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="" />
+          <h1 className=" text-lg lg:text-5xl text-center font-semibold">
+            WELCOME TO MY PORTFOLIO
+          </h1>
+          <img
+            className="h-[20px] lg:h-[50px]"
+            src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg"
+            alt=""
+          />
         </div>
 
-        <div  className="marque flex shrink-0 justify-center items-center gap-10  py-2 px-5 lg:py-5 px-10">
-          <h1 className="text-lg  lg:text-5xl text-center font-bold" style={{ fontFamily: "Saira Condensed, sans-serif" }}>WELCOME TO MY PORTFOLIO</h1>
-          <img className="h-[20px] lg:h-[50px]" src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="" />
+        <div className="marque flex shrink-0 justify-center items-center gap-10  py-2 px-5 lg:py-5 px-10">
+          <h1 className="text-lg  lg:text-5xl text-center font-bold">
+            WELCOME TO MY PORTFOLIO
+          </h1>
+          <img
+            className="h-[20px] lg:h-[50px]"
+            src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg"
+            alt=""
+          />
         </div>
 
-        <div  className="marque flex shrink-0 justify-center items-center gap-10  py-2 px-5 lg:py-5 px-10">
-          <h1 className="text-lg lg:text-5xl text-center font-bold" style={{ fontFamily: "Saira Condensed, sans-serif" }}>WELCOME TO MY PORTFOLIO</h1>
-          <img className="h-[20px] lg:h-[50px]" src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="" />
+        <div className="marque flex shrink-0 justify-center items-center gap-10  py-2 px-5 lg:py-5 px-10">
+          <h1 className="text-lg lg:text-5xl text-center font-bold">
+            WELCOME TO MY PORTFOLIO
+          </h1>
+          <img
+            className="h-[20px] lg:h-[50px]"
+            src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg"
+            alt=""
+          />
         </div>
 
-        <div  className="marque flex shrink-0 justify-center items-center gap-10 py-2 px-5 lg:py-5 px-10 ">
-          <h1 className="text-lg lg:text-5xl text-center font-bold" style={{ fontFamily: "Saira Condensed, sans-serif" }}>WELCOME TO MY PORTFOLIO</h1>
-          <img className="h-[20px] lg:h-[50px]" src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg" alt="" />
+        <div className="marque flex shrink-0 justify-center items-center gap-10 py-2 px-5 lg:py-5 px-10 ">
+          <h1 className="text-lg lg:text-5xl text-center font-bold">
+            WELCOME TO MY PORTFOLIO
+          </h1>
+          <img
+            className="h-[20px] lg:h-[50px]"
+            src="https://www.brandium.nl/wp-content/uploads/2023/07/arrow-br.svg"
+            alt=""
+          />
         </div>
-
       </div>
     </div>
   );
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,177 +1,183 @@
-import React, { useState } from 'react'
-import { motion } from "framer-motion"
-
-
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 
 const Skills = () => {
-  
   const data = [
-    {image:"https://saarthack.github.io/portfolio/images/React-icon.svg.webp"},
-    {image:"https://saarthack.github.io/portfolio/images/java-logo-1.webp"},
-    {image:"https://saarthack.github.io/portfolio/images/js-min.webp"},
-    {image:"https://saarthack.github.io/portfolio/images/html-min.webp"},
-    {image:"https://saarthack.github.io/portfolio/images/css-min.webp"},
-    {image:"https://saarthack.github.io/portfolio/images/gsap-min.webp"},
-    {image:"https://logowik.com/content/uploads/images/tailwind-css3232.logowik.com.webp"}
-  ]
-  
+    { image: "https://saarthack.github.io/portfolio/images/html-min.webp" },
+    { image: "https://saarthack.github.io/portfolio/images/css-min.webp" },
+    { image: "https://saarthack.github.io/portfolio/images/js-min.webp" },
+    {
+      image: "https://saarthack.github.io/portfolio/images/React-icon.svg.webp",
+    },
+
+    { image: "https://saarthack.github.io/portfolio/images/gsap-min.webp" },
+    {
+      image:
+        "https://logowik.com/content/uploads/images/tailwind-css3232.logowik.com.webp",
+    },
+  ];
+
   const data2 = [
-    {image:"https://nodejs.org/static/logos/jsIconGreen.svg"},
-    {image:"https://saarthack.github.io/portfolio/images/mongodb-min.webp"},
-  
-  ]
-  
+    { image: "https://nodejs.org/static/logos/jsIconGreen.svg" },
+    { image: "https://saarthack.github.io/portfolio/images/mongodb-min.webp" },
+  ];
+
   const data3 = [
-    {image:"https://saarthack.github.io/portfolio/images/github-min.webp"},
-    {image:"https://external-preview.redd.it/now-i-have-seen-all-v0-GECZj5NCEqP-3-RG3h5Iaiih6nBAR38ZbxlBrxmk-yw.jpg?auto=webp&s=529d9242cd74c584ff5bfa37e94087814d87ee0b"}
-  ]
-  
+    { image: "https://saarthack.github.io/portfolio/images/github-min.webp" },
+    {
+      image:
+        "https://external-preview.redd.it/now-i-have-seen-all-v0-GECZj5NCEqP-3-RG3h5Iaiih6nBAR38ZbxlBrxmk-yw.jpg?auto=webp&s=529d9242cd74c584ff5bfa37e94087814d87ee0b",
+    },
+    {
+      image: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+    },
+  ];
 
+  const [images, setImages] = useState(data);
 
-const [images, setImages] = useState(data)
+  const [images2, setImages2] = useState(data2);
 
-const [images2, setImages2] = useState(data2)
-
-const [images3, setImages3] = useState(data3)
+  const [images3, setImages3] = useState(data3);
 
   return (
-    <div className='w-full min-h-screen bg-[#222222] pb-10 pt-20 '>
-      <h1 className='text-2xl font-extrabold text-white lg:pt-10  px-5 lg:text-6xl lg:px-14  tracking-normal  md:text-4xl sm:text-3xl' style={{ fontFamily: "china" }}>Things I'm <span className='text-sky-400'>good</span> at</h1>
-      <p className='text-sm font-bold text-white px-6 tracking-wide lg:text-3xl lg:px-16 pt-2'>skills, interest, passion and hobbies</p>
+    <div className="w-full min-h-screen bg-[#222222] pb-10 pt-20 ">
+      <h1 className="skills text-2xl font-extrabold text-white lg:pt-10  px-5 lg:text-6xl lg:px-14  tracking-normal  md:text-4xl sm:text-3xl">
+        Things I'm <span className="text-sky-400">good</span> at
+      </h1>
+      <p className="text-sm font-bold text-white px-6 tracking-wide lg:text-3xl lg:px-16 pt-2">
+        skills, interest, passion and hobbies
+      </p>
 
-      <div className='lg:mt-14 mt-10'>
-        <div className='rounded  bg-gradient-to-r from-[#C33764] to-[#1D2671] max-w-sm whitespace-nowrap py-3 mx-5  lg:max-w-[28%] lg:mx-16 ' >
-          <h2 className='text-xl  text-white px-3 lg:text-3xl lg:px-5 sm:text-2xl md:text-2xl' style={{ fontFamily: "china" }}>Front-end  development</h2>
+      <div className="frontend lg:mt-14 mt-10">
+        <div className="rounded  bg-gradient-to-r from-[#C33764] to-[#1D2671] max-w-sm whitespace-nowrap py-3 mx-5  lg:max-w-[28%] lg:mx-16 ">
+          <h2 className="text-xl  text-white px-3 lg:text-3xl lg:px-5 sm:text-2xl md:text-2xl">
+            Front-end development
+          </h2>
         </div>
 
-        <motion.div className='grid grid-cols-3 lg:flex flex-wrap gap-4 mt-5 mx-9 items-center lg:mx-16 lg:mt-5 '
-        
-        initial={{
-          opacity: 0,
-          x: -50,
-        }}
-
-        whileInView={{
-          opacity: 1,
-          x: 0
-        }}
-
-        transition={{
-          duration: 0.8,
-          delay: 0.3
-
-        }}
-
-        viewport={{
-          once: true
-        }}
-        
-        >
-          {
-            images.map((item, index) => { 
-              return (
-                <div className='w-[70px] h-[70px] bg-white rounded-lg flex items-center justify-center
-                lg:w-[80px] lg:h-[80px]' key={index}>
-                  <img className=' w-[55px] h-[55px] object-cover lg:w-[70px] lg:h-[70px]' src={item.image} alt="" />
-                </div>
-              )
-            })
-          }
-          </motion.div>
-
-      </div>
-
-
-      
-      <div className='mt-14'>
-        <div className='rounded  bg-gradient-to-r from-[#C33764] to-[#1D2671] max-w-sm  whitespace-nowrap py-3 mx-5 lg:max-w-[28%] lg:mx-16' >
-          <h2 className='text-xl  text-white px-3 lg:text-3xl lg:px-5' style={{ fontFamily: "china" }}>Back-end  development</h2>
-        </div>
-
-        <motion.div className='grid grid-cols-3 lg:flex flex-wrap gap-4 mt-5 mx-9 items-center lg:mx-16 lg:mt-5 '
-           
-           initial={{
+        <motion.div
+          className="grid grid-cols-3 lg:flex flex-wrap gap-4 mt-5 mx-9 items-center lg:mx-16 lg:mt-5 "
+          initial={{
             opacity: 0,
-            x: -50
+            x: -50,
           }}
-  
           whileInView={{
             opacity: 1,
-            x: 0
+            x: 0,
           }}
-  
           transition={{
             duration: 0.8,
-            delay: 0.3
-  
+            delay: 0.3,
           }}
-  
           viewport={{
-            once: true
+            once: true,
           }}
-        
         >
-          {
-            images2.map((item, index) => {
-              return (
-                <div className='w-[70px] h-[70px] bg-white rounded-lg flex items-center justify-center
-                lg:w-[80px] lg:h-[80px]' key={index}>
-                  <img className='w-[55px] h-[55px]  lg:w-[65px] h-[65px]' src={item.image} alt="" />
-                </div>
-              )
-            })
-          }
-          </motion.div>
+          {images.map((item, index) => {
+            return (
+              <div
+                className="w-[70px] h-[70px] bg-white rounded-lg flex items-center justify-center
+                lg:w-[80px] lg:h-[80px]"
+                key={index}
+              >
+                <img
+                  className=" w-[55px] h-[55px] object-cover lg:w-[70px] lg:h-[70px]"
+                  src={item.image}
+                  alt=""
+                />
+              </div>
+            );
+          })}
+        </motion.div>
       </div>
 
-
-      <div className='mt-14'>
-        <div className='rounded  bg-gradient-to-r from-[#C33764] to-[#1D2671] max-w-sm whitespace-nowrap py-3 mx-5 lg:max-w-[28%] lg:mx-16' >
-          <h2 className='text-xl  text-white px-3 lg:text-3xl lg:px-5' style={{ fontFamily: "china" }}>Tools & Technologies</h2>
+      <div className="backend mt-14">
+        <div className="rounded  bg-gradient-to-r from-[#C33764] to-[#1D2671] max-w-sm  whitespace-nowrap py-3 mx-5 lg:max-w-[28%] lg:mx-16">
+          <h2 className="text-xl  text-white px-3 lg:text-3xl lg:px-5">
+            Back-end development
+          </h2>
         </div>
 
-        <motion.div className='grid grid-cols-3 lg:flex flex-wrap gap-4 mt-5 mx-9 items-center lg:mx-16 lg:mt-5'
-           
-           initial={{
+        <motion.div
+          className="grid grid-cols-3 lg:flex flex-wrap gap-4 mt-5 mx-9 items-center lg:mx-16 lg:mt-5 "
+          initial={{
             opacity: 0,
-            x: -50
+            x: -50,
           }}
-  
           whileInView={{
             opacity: 1,
-            x: 0
+            x: 0,
           }}
-  
           transition={{
             duration: 0.8,
-            delay: 0.3
-  
+            delay: 0.3,
           }}
-  
           viewport={{
-            once: true
+            once: true,
           }}
-        
-        
         >
-          {
-            images3.map((item, index) => {
-              return (
-                <div className='w-[70px] h-[70px] bg-white rounded-lg flex items-center justify-center
-                lg:w-[80px] lg:h-[80px] ' key={index}>
-                  <img className='w-[55px] h-[55px]  lg:w-[65px] h-[65px] object-cover ' src={item.image} alt="" />
-                </div>
-              )
-            })
-          }
-          </motion.div>
-
+          {images2.map((item, index) => {
+            return (
+              <div
+                className="w-[70px] h-[70px] bg-white rounded-lg flex items-center justify-center
+                lg:w-[80px] lg:h-[80px]"
+                key={index}
+              >
+                <img
+                  className="w-[55px] h-[55px]  lg:w-[65px] h-[65px]"
+                  src={item.image}
+                  alt=""
+                />
+              </div>
+            );
+          })}
+        </motion.div>
       </div>
 
+      <div className="tools mt-14">
+        <div className="rounded  bg-gradient-to-r from-[#C33764] to-[#1D2671] max-w-sm whitespace-nowrap py-3 mx-5 lg:max-w-[28%] lg:mx-16">
+          <h2 className="text-xl  text-white px-3 lg:text-3xl lg:px-5">
+            Tools & Technologies
+          </h2>
+        </div>
 
-
+        <motion.div
+          className="grid grid-cols-3 lg:flex flex-wrap gap-4 mt-5 mx-9 items-center lg:mx-16 lg:mt-5"
+          initial={{
+            opacity: 0,
+            x: -50,
+          }}
+          whileInView={{
+            opacity: 1,
+            x: 0,
+          }}
+          transition={{
+            duration: 0.8,
+            delay: 0.3,
+          }}
+          viewport={{
+            once: true,
+          }}
+        >
+          {images3.map((item, index) => {
+            return (
+              <div
+                className="w-[70px] h-[70px] bg-white rounded-lg flex items-center justify-center
+                lg:w-[80px] lg:h-[80px] "
+                key={index}
+              >
+                <img
+                  className="w-[55px] h-[55px]  lg:w-[65px] h-[65px] object-cover "
+                  src={item.image}
+                  alt=""
+                />
+              </div>
+            );
+          })}
+        </motion.div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Skills
+export default Skills;
